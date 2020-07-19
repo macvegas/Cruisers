cfa27a6a0cf02f904e0320b20329c89068b294f0
Alexandre <alex.delefosse@free.fr>
2020-07-19 21:22:52 +0200

    Tir à finir
    Tir qui fait pas ce qu'on veut mais qui marche :/

---
+++ Assets/Scripts/Events/BoatShootProjectiles.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+
+public class BoatShootProjectiles : MonoBehaviour
+{
+    [SerializeField] private GameObject pfCanonBall;
+
+    private void Awake()
+    {
+        GetComponent<Boat>().OnShoot += BoatShootProjectile_OnShoot;
+    }
+
+    private void BoatShootProjectile_OnShoot(object sender, Boat.OnShootEventArgs e)
+    {
+        Debug.Log("Shots fired!");
+        //Shoot
+        GameObject projectileTransform = Instantiate(pfCanonBall, e.canonPosition, Quaternion.identity);
+
+        Vector3 shootDir = (e.shootPosition - e.canonPosition).normalized;
+        projectileTransform.GetComponent<CanonBall>().Setup(shootDir);
+    }
+}
---
+++ Assets/Scripts/Weapons/Weapon.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Diagnostics;
+using UnityEngine;
+
+public abstract class Weapon : MonoBehaviour
+{
+    public float damage;
+    public float coolDownTime;
+    public bool canShoot;
+    public float projectileSpeed;
+    public float projectileCount;
+    public Vector3 direction;
+
+    protected abstract void Shoot();
+
+    public void TryShoot()
+    { 
+        if(canShoot)
+        {
+            Shoot();
+            StartCoroutine(StartCoolDown());
+        }
+    }
+
+    private IEnumerator StartCoolDown()
+    {
+        canShoot = false;
+
+        float time = 0f;
+
+        while (time < coolDownTime)
+        {
+            //TODO rafraichir l'affichage pour montrer cb de temps il reste
+            time += Time.deltaTime;
+            yield return null;
+        }
+        canShoot = true;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
